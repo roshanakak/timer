c409068c95ee8d5d25b124693f975cc20cc6be23
Roshanak Akbarifar <roshanak.akbarifar@gmail.com>
2021-06-28 20:50:54 +0000

    timer1.js modified

--- timer1.js
+++ timer1.js
@@ -1,16 +1,20 @@
 const timer1 = function() {
   let args = process.argv.splice(2, process.argv.length);
   args.forEach(function(element, index) {
-    args[index] = parseInt(element);
+    if (!isNaN(element)) args[index] = parseInt(element);
   }, args);
-  args = args.sort(function(a, b){return a - b});
+  args = args.sort(function(a, b) {
+    return a - b;
+  });
 
   let delay = 0;
   args.forEach(element => {
-    delay = element * 1000;
+    if (element > 0) {
+      delay = element * 1000;
       setTimeout(() => {
         process.stdout.write('\x07');
       }, delay);
+    }
   });
 };
 
